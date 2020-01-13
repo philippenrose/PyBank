2411bbe1d2c432cc164a3c786eebf96c15c9c75a
Philip Penrose <46907267+philippenrose@users.noreply.github.com>
2020-01-13 10:56:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,20 +25,7 @@ def PyBank(Budget):
         Greatest_Decrease
     
 
-   
 
-
-    #print(Profit_Losses)
-    print(f'Financial Analysis')
-    print(f'------------------------')
-    print(f'Total Months: {Month}')
-    print(f'Total Profit/Losses: ${Total_Profit_Losses}')
-    print(f'Average Change: ${Average_Change}')
-    print(f'Greatest Increase: {Date_I} with ${Greatest_Increase}')
-    print(f'Greatest Decrease: {Date_D} with ${Greatest_Decrease}')
-
-with open(csv_path, newline="") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
 
     #print(csv_file)
 
