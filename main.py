69fc24bc0be33e28ba7d485706ba2abe094a0be0
Philip Penrose <46907267+philippenrose@users.noreply.github.com>
2020-01-13 10:53:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import os
 import csv
 
 csv_path = os.path("C:\Users\Home\WUSTL201907DATA2\03-Python\Homework\Instructions\PyBank\Resources\budget_data.csv")
+
+def PyBank(Budget):
+    Month = 0
+    Month_List = []
+    Profit_Losses = []
+    Change_List = []
+    
+    
